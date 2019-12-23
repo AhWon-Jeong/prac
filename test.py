fe4238c3a0503e901a2bd2c8701891cad5204952
AhWon-Jeong <hy971216@naver.com>
2019-12-24 05:24:53 +0900

    conn

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-from bs4 import BeautifulSoup
+# sql 연결/해제
+
+import pymysql
+
+conn = pymysql.connect(
+        host = " ", user = " ", password = " ", database = " ", charset='utf8mb4'
+)
+
+try:
+    cur = conn.cursor()
+    cur.execute(
+        '''
+        명령어
+        '''
+    )
+
+finally:
+    cur.close()
+    conn.close()
